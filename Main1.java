09c79a85d42e08f64621d125f2051771e6d7ceaa
syam-jason <55087719+syam-jason@users.noreply.github.com>
2019-12-23 16:17:21 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,36 @@
+ import java.util.*;
+ 
+
+public class Main1
+
+{
+	
+public static void main(String[] args)
+	
+{
+		
+Scanner sc=new Scanner(System.in);
+		
+int a[]=new int[10];
+		
+int sum=0;
+		
+int len=a.length;
+		
+for(int i=0;i<10;i++)
+		
+{
+		    
+a[i]=sc.nextInt();
+		  
+sum=sum+a[i];
+		
+}
+		
+System.out.println("The Sum of array is :"+sum);
+		
+System.out.println("The Average of the array is :"+(sum/len));
+	
+}
+s
+}
